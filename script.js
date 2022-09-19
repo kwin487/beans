0d688c49a03b1f4bf868f1ae641e47c064406abc
kwin487 <82954361+kwin487@users.noreply.github.com>
2022-09-19 14:03:54 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-//this is mainly intended as a prank program for friends or as a type of revenge on another person, but this might still be kinda deadly to very low end computers.
 
-function NewTab() {
-            window.open(
-            "index.html", "_blank","width=1920,height=1080");
-        }
-
-for (var i = 0; i < Infinity; i++) {
-       NewTab();
-}
